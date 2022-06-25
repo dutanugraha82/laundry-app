a327f787ceeafc5c2df6925863f4fe3d3963821e
muhamadijlal_ <haidarijlal027@gmail.com>
2022-06-25 12:24:09 +0700

    fixed tambah input row

--- resources/views/input-data.blade.php
+++ resources/views/input-data.blade.php
@@ -9,7 +9,7 @@
         <div class="card-body">
             <form action="/input-data-laundry" method="POST">
                 @csrf
-                <div class="container my-3">
+                <div class="container my-3" id="rowRoot">
                     <div class="row">
                         <div class="col">
                             <div class="mb-3">
@@ -72,19 +72,19 @@
                             </div>
                         </div>
                     </div>
-                    <div class="row" id="removeRow">
+                    <!-- <div class="row" id="removeRow">
                         <div class="col" id="col1"></div>
                         <div class="col" id="col2"></div>
-                    </div>
-                    <div class="row">
-                        <div class="col">
-                            <div class="mb-3">                                
-                                <input type="text" class="form-control" name="status_pembayaran" value="" hidden>
-                            </div>
-                            <div class="my-3">
-                                <button type="submit" class="btn btn-primary">Submit Data Laundry</button>
-                                <button id="addRow" type="button" class="btn btn-success">Tambah barang</button>
-                            </div>
+                    </div> -->
+                </div>
+                <div class="row">
+                    <div class="col">
+                        <div class="mb-3">                                
+                            <input type="text" class="form-control" name="status_pembayaran" value="" hidden>
+                        </div>
+                        <div class="my-3">
+                            <button type="submit" class="btn btn-primary">Submit Data Laundry</button>
+                            <button id="addRow" type="button" class="btn btn-success">Tambah barang</button>
                         </div>
                     </div>
                 </div>
@@ -104,40 +104,30 @@
 <script type="text/javascript">
     // add row
     $("#addRow").click(function () {
-        var html = '';
-        html += '<div class="mb-3" id="inputFormRow">';
-        html += '<label for="jenis">jenis Laundry</label>';
-        html += '<select name="jenis[]" id="jenis" class="form-control @error("jenis") is-invalid @enderror" required>';
-        html += '<option value="">--Pilih jenis Laundry--</option>';
-        html += '@foreach ($dataNamaJenis as $data)';
-        html += '<option value="{{$data->nama_jenis}}">{{ $data->nama_jenis }}</option>';
-        html += '@endforeach';
-        html += '@error("jenis")';
-        html += '<span id="exampleInputEmail1-error" class="error invalid-feedback">{{$message}}</span>';
-        html += '@enderror';
-        html += '</select>';
-        html += '</div>';        
+        var html = '';      
         
-        $('#col1').append(html);
-    });
-
-    $("#addRow").click(function () {
-        var html = '';
-        html += '<div class="mb-3" id="inputFormRow">';
-        html += '<label for="qty">Qty <span style="color: red; font-size: 12px; font-weight: normal;" >(*Kg *Meter *Pasang *Pcs)</span></label>';
-        html += '<input type="text" name="qty[]" class="form-control @error("qty") is-invalid @enderror" id="qty" value="{{ old("qty") }}">';        
-        html += '@error("qty")';
-        html += '<span id="exampleInputEmail1-error" class="error invalid-feedback">{{$message}}</span>';
-        html += '@enderror';
-        html += '<a id="removeRow" type="submit" style="color: red;">- Hapus Row</a>';
+        html += '<div class="row" id="rowInputAdd">';
+        html += '<div class="col">';
+        html += '<div class="mb-3">';
+        html += '<label for="no_transaksi">Nomor Transaksi</label>';
+        html += '<input type="text" class="form-control" name="no_transaksi" id="no_transaksi" value="001" readonly>'
         html += '</div>';
-
-        $('#col2').append(html);
-    });    
-
+        html += '</div>';
+        html += '<div class="col">';
+        html += '<div class="mb-3">';
+        html += '<label for="nohp">No Hp</label>';
+        html += '<input type="text" class="form-control @error("nohp") is-invalid @enderror" name="nohp" id="nohp" value="{{ old("nohp") }}">';
+        html += '<a type="button" id="deleteRowInput"> - Hapus Row Input</a>';
+        html += '</div>';          
+        html += '</div>';          
+        html += '</div>';           
+        
+        $('#rowRoot').append(html);
+    });
+   
     // remove row
-    $(document).on('click', '#removeRow', function () {
-        $('#removeRow').remove();
+    $(document).on('click', '#deleteRowInput', function () {
+        $(this).closest('#rowInputAdd').remove();
     });
 </script>
 @endpush
